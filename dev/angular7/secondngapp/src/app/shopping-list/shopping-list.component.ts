21937b6ac30bd742ec628880892e1f84a16708ae
BradHuang <adghbc01@tamu.edu>
2019-03-22 00:42:07 +0000

    more components added.

--- dev/angular7/secondngapp/src/app/shopping-list/shopping-list.component.ts
+++ dev/angular7/secondngapp/src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Peppers', 3),
+    new Ingredient('Tomatoes', 2)
+  ];
+  
   constructor() { }
 
   ngOnInit() {
