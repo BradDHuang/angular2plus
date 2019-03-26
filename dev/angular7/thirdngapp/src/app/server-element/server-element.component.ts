728719309b4a30ca75aa0a491a039819ff7fa93c
BradHuang <adghbc01@tamu.edu>
2019-03-26 05:59:51 +0000

    Lifecycle Hooks.

--- dev/angular7/thirdngapp/src/app/server-element/server-element.component.ts
+++ dev/angular7/thirdngapp/src/app/server-element/server-element.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { 
+  Component, OnInit, 
+  Input, ViewEncapsulation, 
+  OnChanges, SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+  ViewChild, ElementRef,
+  ContentChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,16 +18,60 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated // None, Native
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements 
+  OnInit, 
+  OnChanges, 
+  DoCheck, 
+  AfterContentInit,
+  AfterContentChecked,
+  OnDestroy {
   @Input('srvElement') element: {
     type: string,
     name: string,
     content: string
   };
+  @Input() name: string;
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentP') p: ElementRef;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() { 
+    console.log('constructor get called');
+  }
+  
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges get called');
+    // console.log(changes);
   }
 
+  ngOnInit() {
+    console.log('ngOnInit get called');
+    console.log('T C: ' + this.header.nativeElement.textContent);
+    console.log('p T: ' + this.p.nativeElement.textContent);
+  }
+  
+  ngDoCheck() {
+    console.log('ngDoCheck get called');
+  }
+  
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit get called');
+    console.log('p T: ' + this.p.nativeElement.textContent);
+  }
+  
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked get called');
+  }
+  
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit get called');
+    console.log('T C: ' + this.header.nativeElement.textContent);
+  }
+  
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked get called');
+  }
+  
+  ngOnDestroy() {
+    console.log('ngOnDestroy get called');
+  }
 }
