e900ee8db128a71a75a020d44a3c4a56cb1e4666
BradHuang <adghbc01@tamu.edu>
2019-04-01 04:05:33 +0000

    Add Ingredients to Recipes

--- dev/angular7/secondngapp/src/app/recipes/recipe.service.ts
+++ dev/angular7/secondngapp/src/app/recipes/recipe.service.ts
@@ -1,10 +1,21 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     private recipes: Recipe[] = [
-        new Recipe('Salsa', 'Strawberry salsa', 'https://cookieandkate.com/images/2017/03/strawberry-salsa-recipe.jpg'),
-        new Recipe('Salsa2', 'Strawberry salsa', 'https://cookieandkate.com/images/2017/03/strawberry-salsa-recipe.jpg')
+        new Recipe('Salsa', 'Strawberry salsa', 'https://cookieandkate.com/images/2017/03/strawberry-salsa-recipe.jpg',
+                   [
+                       new Ingredient('Strawberry', 6),
+                       new Ingredient('Onion', 4)
+                   ]
+        ),
+        new Recipe('Spicy-Salsa', 'Pepper salsa', 'https://www.gimmesomeoven.com/wp-content/uploads/2017/05/Restaurant-Style-Salsa-Recipe-5.jpg',
+                   [
+                       new Ingredient('Green Pepper', 3),
+                       new Ingredient('Red Pepper', 8)
+                   ]
+        )
     ];
     
     recipeSelected = new EventEmitter<Recipe>();
@@ -13,5 +24,4 @@ export class RecipeService {
         // return this.recipes;
         return this.recipes.slice(); // A new array.
     }
-    
 }
