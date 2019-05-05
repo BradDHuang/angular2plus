4b49d43a57d1bf522ab8f85d02b96ae54db5a8b2
BradHuang <adghbc01@tamu.edu>
2019-05-05 22:13:03 +0000

    POST req example

---
+++ dev/angular7/servicesapp/src/app/server.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Injectable()
+export class ServerService {
+    constructor(private http: Http) {}
+    
+    storeServers(servers: any[]) {
+        return this.http.post('https://ng-http-aaee8.firebaseio.com/data.json', servers);
+    }
+}
