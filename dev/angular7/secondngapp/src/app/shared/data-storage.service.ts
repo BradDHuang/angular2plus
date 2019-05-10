421c460b3756beae7a40924ef863017c2616eb0c
BradHuang <adghbc01@tamu.edu>
2019-05-10 23:17:17 +0000

    Fetching Recipes with http.get()

--- dev/angular7/secondngapp/src/app/shared/data-storage.service.ts
+++ dev/angular7/secondngapp/src/app/shared/data-storage.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class DataStorageService {
@@ -12,6 +15,29 @@ export class DataStorageService {
         return this.http.put('https://ng-recipes-d1765.firebaseio.com/recipes.json',
                       this.recipeService.getRecipes()
         );
+    }
+    
+    getRecipes() {
+        this.http.get('https://ng-recipes-d1765.firebaseio.com/recipes.json')
+            .pipe(map(
+                (res: Response) => {
+                    const recipes: Recipe[] = res.json();
+                    // check if there is an ingredients prop.
+                    for (let recipe of recipes) {
+                        if (!recipe['ingredients']) {
+                            // console.log(recipe);
+                            recipe['ingredients'] = [];
+                        }
+                    }
+                    return recipes;
+                }
+            ))
+            .subscribe(
+                (recipes: Recipe[]) => {
+                    // const recipes: Recipe[] = res.json();
+                    this.recipeService.setRecipes(recipes);
+                }
+            );
     }
     
 }
