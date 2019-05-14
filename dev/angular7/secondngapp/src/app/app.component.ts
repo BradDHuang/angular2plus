1dd64debaa9bf57b53d7f3c0122391bc48882fc1
BradHuang <adghbc01@tamu.edu>
2019-05-14 03:27:22 +0000

    user sign up with Firebase SDK

--- dev/angular7/secondngapp/src/app/app.component.ts
+++ dev/angular7/secondngapp/src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   /*
   // title = 'secondngapp';
   loadedFeature = 'recipe';
@@ -14,4 +15,11 @@ export class AppComponent {
     this.loadedFeature = feature;
   }
   */
+  
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCUolZi4uCXr4fcYLeITLQOjo_Zx4qmU2Y",
+      authDomain: "ng-http-aaee8.firebaseapp.com"
+    });
+  }
 }
